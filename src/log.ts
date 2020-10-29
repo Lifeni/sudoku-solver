8352dd00338bacdf8ac94256478a092e91daa723
梁峰宁 <liangfengning@foxmail.com>
2020-10-29 09:50:20 +0800

    Fix logs path

--- src/log.ts
+++ src/log.ts
@@ -1,8 +1,9 @@
 import fs from "fs"
+import os from "os"
 import tracer from "tracer"
 import path from "path"
 
-const logName = path.resolve("./sudoku-solver.log")
+const logName = path.resolve(os.tmpdir(), "sudoku-solver.log")
 
 const log = tracer.console({
     transport: function (data) {
@@ -15,7 +16,12 @@ const log = tracer.console({
     }
 })
 
+if (!fs.existsSync(logName)) {
+    log.log("Create Log File")
+}
+
 const show = async () => {
+    console.log("\nLogs", logName, "\n")
     try {
         const logs = fs.readFileSync(logName)
         console.log(logs.toString())
